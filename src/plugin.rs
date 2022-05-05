3387f93dce23f7a37c66dae4c1510f09d7e6c1f3
SquitchYT <clementfoure2005@gmail.com>
2022-05-05 22:04:28 +0200

    Adding title() method Removing refresh_rate() method Renaming update() method to on_update()

--- src/plugin.rs
+++ src/plugin.rs
@@ -1,7 +1,7 @@
 pub trait Plugin {
     fn display(&mut self, height: i32, width: i32) -> String;
+    fn title(&mut self) -> String { String::from("") }
 
-    fn update(&mut self) {} // called every time the loop for ac tualize data is done
-    fn refresh_rate(&self) -> i32 { 333 } // set the time between two refresh loop execution
-    fn on_input(&mut self, _key: String) { }
+    fn on_update(&mut self) {}
+    fn on_input(&mut self, _key: String) {}
 }
